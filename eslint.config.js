da725978c6913d53b26ab96a3c23405a3ddd5137
matuidi1234 <nzundiecedric@yahoo.com>
2025-06-19 23:11:53 +0200

    Quête 2 complétée - Tests automatisés et couverture de code

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,8 @@ import globals from 'globals';
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
-    ignores: ["**/node_modules/**"],
+    files: ['**/*.js'],
+    ignores: ['**/node_modules/**'],
     languageOptions: {
       globals: {
         ...globals.node,
@@ -16,12 +16,12 @@ export default [
       sourceType: 'module'
     },
     rules: {
-      "indent": ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
-      "no-console": "warn",
-      "eqeqeq": "error"
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-console': 'warn',
+      'eqeqeq': 'error'
     }
   }
 ];
