d416277e2e1b574a1ab58d52405cd42a692bfb15
matuidi1234 <nzundiecedric@yahoo.com>
2025-06-19 23:00:04 +0200

    Quête 2 complétée - Tests automatisés et couverture de code

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+// .eslintrc.cjs
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "no-console": "warn"
+  }
+};
